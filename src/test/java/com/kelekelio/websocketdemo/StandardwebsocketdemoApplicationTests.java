d8faac147b88fd673f5163535ea0a75f6ed27321
Kelekelio <cristal01@gmail.com>
2023-11-05 14:43:38 +0100

    init

--- src/test/java/com/kelekelio/websocketdemo/StandardwebsocketdemoApplicationTests.java
+++ src/test/java/com/kelekelio/websocketdemo/StandardwebsocketdemoApplicationTests.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.websocket.WsWebSocketContainer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import com.neovisionaries.ws.client.*;
 
 import java.net.URI;
 
@@ -26,6 +28,7 @@ class WebsocketdemoApplicationTests {
 
         @Test
         void subscribe() throws Exception {
+            log.info("Current Thread: {}", Thread.currentThread().getId());
             StandardWebSocketClient client = new StandardWebSocketClient(new WsWebSocketContainer());
             WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
             WebSocketSession session = client.execute(handler, headers, URI.create(String.format("ws://localhost:%d/websocket/%d", port, 1L))).get();
@@ -33,5 +36,15 @@ class WebsocketdemoApplicationTests {
             log.info("Session Id in test: {}, URI: {}", session.getId(), session.getUri());
             Assertions.assertNotNull(session.getUri());
             Assertions.assertFalse(session.getAttributes().isEmpty());
+        }
+
+        @Test
+        void sunscribe2() throws Exception {
+            WebSocket session = new WebSocketFactory()
+                    .createSocket("http://localhost:" + port + "/websocket/1")
+                    .connect();
+
+            Assertions.assertTrue(session.isOpen());
+            Assertions.assertNotNull(session.getURI());
         }
 }
