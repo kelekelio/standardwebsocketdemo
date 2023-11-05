3f7c8669ebc2b0e40887a41fe5fd21bf70f1b5b2
Kelekelio <cristal01@gmail.com>
2023-11-05 11:19:43 +0100

    init

---
+++ src/main/java/com/kelekelio/websocketdemo/ServerWebSocketHandler.java
@@ -0,0 +1,62 @@
+package com.kelekelio.websocketdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.SubProtocolCapable;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Slf4j
+public class ServerWebSocketHandler extends TextWebSocketHandler implements SubProtocolCapable {
+
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        log.info("======================");
+        log.info("Session established connection: {}, URI: {}, Thread id: {}", session.getId(), session.getUri(), Thread.currentThread().getId());
+        log.info("======================");
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        log.info("Server connection closed: {}", status);
+        sessions.remove(session);
+    }
+
+    @Scheduled(fixedRate = 10000)
+    void sendPingMessage() throws IOException {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                String broadcast = "server ping " + LocalTime.now();
+                log.info("Server sends: {}", broadcast);
+                session.sendMessage(new TextMessage(broadcast));
+            }
+        }
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        log.info("Server transport error: {}", exception.getMessage());
+    }
+
+    @Override
+    public List<String> getSubProtocols() {
+        return Collections.singletonList("subprotocol.kelekelio.websocketdemo");
+    }
+}
--- src/test/java/com/kelekelio/websocketdemo/StandardwebsocketdemoApplicationTests.java
+++ src/test/java/com/kelekelio/websocketdemo/StandardwebsocketdemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.kelekelio.websocketdemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.websocket.WsWebSocketContainer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
-@SpringBootTest
-class StandardwebsocketdemoApplicationTests {
+import java.net.URI;
 
-    @Test
-    void contextLoads() {
-    }
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Slf4j
+class WebsocketdemoApplicationTests {
 
+        @Autowired private ServerWebSocketHandler handler;
+        @LocalServerPort
+        private int port;
+
+
+        @Test
+        void subscribe() throws Exception {
+            StandardWebSocketClient client = new StandardWebSocketClient(new WsWebSocketContainer());
+            WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+            WebSocketSession session = client.execute(handler, headers, URI.create(String.format("ws://localhost:%d/websocket/%d", port, 1L))).get();
+
+            log.info("Session Id in test: {}, URI: {}", session.getId(), session.getUri());
+            Assertions.assertNotNull(session.getUri());
+        }
 }
