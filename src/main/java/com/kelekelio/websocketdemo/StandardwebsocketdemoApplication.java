0665a592033163b42673a7c7ed433e8accc0c024
Kelekelio <cristal01@gmail.com>
2023-11-05 11:13:28 +0100

    Initial commit

---
+++ src/main/java/com/kelekelio/websocketdemo/StandardwebsocketdemoApplication.java
@@ -0,0 +1,13 @@
+package com.kelekelio.websocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardwebsocketdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardwebsocketdemoApplication.class, args);
+    }
+
+}
